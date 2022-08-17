8fa8ec772015cd527915f6163d433e0ae1662e48
Mariana Cerda <marianacerdaaldasoro@gmail.com>
2022-08-16 20:09:52 -0500

    agregando storage y modificando css

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function Item (id, type, size, price, material, description, condition) {
-  this.id = id;
-  this.type = type;
-  this.size 	 = size;
-  this.price  = price;
-  this.material = material;
-  this.description 	 = description;
-  this.condition  = condition;
-}
-
 let validConditions = {
   usado: true,
   seminuevo: true,
@@ -19,6 +9,23 @@ let id = 0;
 let itemArray = [];
 
 let container = document.querySelector ('.container');
+let button = document.querySelector('button');
+let type = document.querySelector('#type');
+let size = document.querySelector('#size');
+let price = document.querySelector('#price');
+let material = document.querySelector('#material');
+let description = document.querySelector('#description');
+let condition = document.querySelector('#condition');
+
+function Item (id, type, size, price, material, description, condition) {
+  this.id = id;
+  this.type = type;
+  this.size 	 = size;
+  this.price  = price;
+  this.material = material;
+  this.description 	 = description;
+  this.condition  = condition;
+}
 
 function filterCondition (condition, itemArray) {
   let con= condition
@@ -29,130 +36,61 @@ function filterCondition (condition, itemArray) {
   return filterArray
 }
 
-function defineProperty (firstPrompt, wrongInputPrompt, whileCondition)  {
-  let property = prompt (firstPrompt);
-
-  while (whileCondition(property)) {
-    property = prompt (wrongInputPrompt);
-  }
-
-  return property
-}
-
 function isString(promptInput) {
   return isNaN(Number(promptInput));
 }
 
-function createItem () {
-  id++;
-
-  let type = defineProperty(
-    'Agregar tipo de prenda para ninio',
-    'Este nos es un tipo de prenda, ingresar un tipo de prenda correcto',
-    function (property) {
-      return !isString(property)
-    }
-  )
-
-  let size = defineProperty(
-    'Agregar talla de la prenda en valor numerico',
-    'Este no es una talla valida, favor de agregarlo en valor numerico, entre el 2 y 16',
-    function (property) {
-      return isString(property) || (Number(property) % 2 != 0 || Number(property) > 16)
-    }
-  )
-
-  let price = defineProperty(
-    'Agregar el precio en que se publicara tu producto',
-    'Este no es un precio adecuado, favor de agregar un precio correcto',
-    function (property) {
-      return isString(property) || (Number(property) <= 0)
-    }
-  )
-
-  let material = defineProperty(
-    'Agregar el material de la prenda',
-    'Este no es un material adecuado, favor de agregar un material correcto',
-    function (property) {
-      return !isString(property)
-    }
-  )
-
-  let description = defineProperty(
-    'Agregar la descripcion de la prenda, lo mas detallado posible',
-    'Esta no es una descripcion adecuada, favor de agregar una descripcion correcta',
-    function (property) {
-      return !isString(property)
-    }
-  )
-
-
-  let condition = defineProperty(
-    'Agregar la condicion de la prenda, definirla como: usado, seminuevo o nuevo',
-    'Este no es un condition adecuada, favor de agregar una condicion correcta, ya sea usado, seminuevo o nuevo',
-    function (property) {
-      return !isString(property) || !validConditions[property]
-    }
-  )
-
-  let item = new Item(id, type, size, price, material, description, condition);
-
-  return item;
+function agregarHtml (item, container) {
+  container.innerHTML += `
+    <div class="card" style="width: 18rem;">
+      <img src="https://picsum.photos/200/200" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${item.type}</h5>
+        <p class="card-text">${item.description}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Talla: ${item.size}</li>
+        <li class="list-group-item">Material: ${item.material}</li>
+        <li class="list-group-item">Precio: $${item.price}</li>
+      </ul>
+    </div>
+  `;
 }
-
-let button = document.querySelector('button');
-
-let type = document.querySelector('#type');
-
-console.log (type)
-
-let size = document.querySelector('#size');
-
-let price = document.querySelector('#price');
-
-let material = document.querySelector('#material');
-
-let description = document.querySelector('#description');
-
-let condition = document.querySelector('#condition');
 
 function fnClick(event) {
+  event.preventDefault();
+  let newitem = new Item()
+  newitem.id = Math.floor(Math.random() * 99999999)
+  newitem.type = type.value
+  newitem.size = size.value
+  newitem.price = price.value
+  newitem.material = material.value
+  newitem.description = description.value
+  newitem.condition = condition.value
 
-let newitem = new Item()
+  if (!localStorage.length) {
+    localStorage.setItem('items', JSON.stringify([]))
+  }
 
-newitem.type = type.value
-newitem.size = size.value
-newitem.price = price.value
-newitem.material = material.value
-newitem.description = description.value
-newitem.condition = condition.value
-console.log (newitem)
+  let itemsArray = JSON.parse(localStorage.getItem('items'));
+  itemsArray.push(newitem);
 
-itemArray.push(newitem);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
 
-
-  console.log (newitem)
-  container.innerHTML += `
-  <div class="card" style="width: 18rem;">
-  <img src="https://picsum.photos/200/200" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${newitem.type}</h5>
-    <p class="card-text">${newitem.description}</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Talla: ${newitem.size}</li>
-    <li class="list-group-item">Material: ${newitem.material}</li>
-    <li class="list-group-item">Precio: $${newitem.price}</li>
-  </ul>
-</div>
-  `
-;
-
-
+// items.push(newitem);
+  agregarHtml(newitem, container);
 }
 
 
+function initialLoad() {
+  let storageItems = JSON.parse(localStorage.getItem('items')) || [];
 
+  for (let item of storageItems) {
+    agregarHtml(item, container);
+  }
+}
+
+initialLoad();
 button.addEventListener("click", fnClick);
 
 
